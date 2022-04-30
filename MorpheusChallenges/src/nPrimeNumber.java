32c1c615784ffdb062c5b1cfb8d54007a1185b32
Alexander <alexander.fink@uni-ulm.de>
2022-04-30 13:47:05 +0200

    .gitignore changes

---
+++ MorpheusChallenges/src/nPrimeNumber.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class nPrimeNumber {
+
+    public nPrimeNumber() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter writer = new PrintWriter(System.out);
+
+        
+    }
+
+    public static void main(String[] args) {
+        new nPrimeNumber();
+    }
+}
